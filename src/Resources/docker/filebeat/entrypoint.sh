e68b698ab992a7d94c0340e1ec596b634fd04c18
Jordan <jordanleeboland@gmail.com>
2020-02-17 08:31:36 -0500

    Auto-generating index and dashboards

--- src/Resources/docker/filebeat/entrypoint.sh
+++ src/Resources/docker/filebeat/entrypoint.sh
@@ -13,4 +13,22 @@ done
 #kibana reports ready slightly before the API actually responds to connections
 sleep 10
 
+while IFS="" read -r serviceName || [ -n "$serviceName" ]
+do
+  printf '%s\n' "$serviceName"
+  sed "s/SERVICENAME/$serviceName/g" dashboard_template.json > $serviceName.json
+
+  UUID_1=$(cat /proc/sys/kernel/random/uuid)
+  UUID_2=$(cat /proc/sys/kernel/random/uuid)
+  UUID_3=$(cat /proc/sys/kernel/random/uuid)
+
+  sed -i "s/UUID_1/$UUID_1/g" $serviceName.json
+  sed -i "s/UUID_2/$UUID_2/g" $serviceName.json
+  sed -i "s/UUID_3/$UUID_3/g" $serviceName.json
+
+  curl -XPOST kibana:5601/api/kibana/dashboards/import -H 'kbn-xsrf:true' -H 'Content-type:application/json' -d @$serviceName.json
+
+done < service_list
+
+
 ./filebeat -c filebeat.yml -e
